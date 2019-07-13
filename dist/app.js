91d7521c763fcc55819a8cc0bac8326f6629f70d
krishna verma <thisistech5@gmail.com>
2019-07-14 01:59:26 +0530

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const para = document.querySelector('p');
+
+button.addEventListener('click', () => {
+    para.textContent = 'Hello Boiis ....';
+})
